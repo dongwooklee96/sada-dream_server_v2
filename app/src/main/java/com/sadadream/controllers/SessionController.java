cfa8f3f4f67d8ee13e6dd591561bfea1b72c9897
Lee Dong Wook <sh95119@gmail.com>
2021-07-06 00:16:48 +0900

    DOCS: 로그인 컨트롤러 및 데이터에 API 문서를 위한 어노테이션 추가

--- app/src/main/java/com/sadadream/controllers/SessionController.java
+++ app/src/main/java/com/sadadream/controllers/SessionController.java
@@ -5,9 +5,15 @@ import com.sadadream.dto.SessionRequestData;
 import com.sadadream.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ServerErrorException;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
-@RequestMapping("/session")
+@RequestMapping(value = "/session", produces = "application/json")
 @CrossOrigin
 public class SessionController {
     private final AuthenticationService authenticationService;
@@ -16,6 +22,7 @@ public class SessionController {
         this.authenticationService = authenticationService;
     }
 
+    @ApiOperation(value="로그인", notes = "로그인을 수행한다", response = SessionResponseData.class)
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login(
