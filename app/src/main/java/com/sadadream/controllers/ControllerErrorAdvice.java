7c0df32684748cd1f37b84016ae7a4792e1f2e93
Lee Dong Wook <sh95119@gmail.com>
2021-07-07 23:30:07 +0900

    FIX: 컨트롤러에서 에러 발생시 원활한 추적을 위해 발생한 날짜 및 에러 메시지를 추가하였다

--- app/src/main/java/com/sadadream/controllers/ControllerErrorAdvice.java
+++ app/src/main/java/com/sadadream/controllers/ControllerErrorAdvice.java
@@ -7,13 +7,17 @@ import com.sadadream.errors.UserEmailDuplicationException;
 import com.sadadream.errors.UserNotFoundException;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+
+import java.util.Date;
 import java.util.Set;
 
 @ResponseBody
@@ -21,36 +25,43 @@ import java.util.Set;
 public class ControllerErrorAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
-    public ErrorResponse handleProductNotFound() {
-        return new ErrorResponse("Product not found");
+    public ErrorResponse handleProductNotFound(Exception exception, WebRequest request) {
+        return new ErrorResponse(new Date(), exception.getMessage(), request.getDescription(false));
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
-    public ErrorResponse handleUserNotFound() {
-        return new ErrorResponse("User not found");
+    public ErrorResponse handleUserNotFound(Exception exception, WebRequest request) {
+        return new ErrorResponse(new Date(), exception.getMessage(), request.getDescription(false));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserEmailDuplicationException.class)
-    public ErrorResponse handleUserEmailIsAlreadyExisted() {
-        return new ErrorResponse("User's email address is already existed");
+    public ErrorResponse handleUserEmailIsAlreadyExisted(Exception exception, WebRequest request) {
+        return new ErrorResponse(new Date(), exception.getMessage(), request.getDescription(false));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(LoginFailException.class)
-    public ErrorResponse handleLoginFailException() {
-        return new ErrorResponse("Log-in failed");
+    public ErrorResponse handleLoginFailException(Exception exception, WebRequest request) {
+        return new ErrorResponse(new Date(), exception.getMessage(), request.getDescription(false));
     }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException.class)
     public ErrorResponse handleConstraintValidateError(
-            ConstraintViolationException exception
+            ConstraintViolationException exception, WebRequest request
     ) {
         String messageTemplate = getViolatedMessage(exception);
-        return new ErrorResponse(messageTemplate);
+        return new ErrorResponse(new Date(), messageTemplate, request.getDescription(false));
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleMethodArgumentNotValidError(Exception exception, WebRequest request) {
+        return new ErrorResponse(new Date(), exception.getMessage(), request.getDescription(false));
     }
 
     private String getViolatedMessage(ConstraintViolationException exception) {
