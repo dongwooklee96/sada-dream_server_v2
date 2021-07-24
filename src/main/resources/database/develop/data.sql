68028be9385b8dbca99ee434686ded21624888f7
Lee Dong Wook <sh95119@gmail.com>
2021-07-25 03:25:17 +0900

    CHORE: 서버 실행시 자동으로 실행되는 SQL 스크립트 내용 추가
    유저와 권한을 추가하였다

--- src/main/resources/database/develop/data.sql
+++ src/main/resources/database/develop/data.sql
@@ -1,1 +1,5 @@
-SELECT 1;
+INSERT INTO "user" values (1, '경기도 안양시', '1996-01-06', false, 'sh95119@gmail.com', 'M', '이동욱', '$2a$10$ux33yAnMEyyRnkaBZ21tfOLDwbbVyuGy3kV5H0cS45Q46ctY4/zq.', '01040621403');
+INSERT INTO "user" values (2, '서울특별시', '2000-01-06', false, 'system@sys.com', 'M', '관리자', '$2a$10$0LO1RgK2q.M22TcSeI1jKud0k0iKhQO5JJU3P/qcgg9w5.A4j/Zt.', '01077777777');
+
+INSERT INTO "role" values(1, 'USER', 1);
+INSERT INTO "role" values(2, 'ADMIN', 2);
