255a4b76fd2eb5b7c868b42dd8766f8b57804b64
Lee Dong Wook <sh95119@gmail.com>
2021-08-01 01:21:20 +0900

    FEAT: 엔티티 생성, 수정 시간 자동화

--- src/main/java/com/sadadream/App.java
+++ src/main/java/com/sadadream/App.java
@@ -4,14 +4,17 @@ import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class App {
 
